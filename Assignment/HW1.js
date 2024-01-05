dd72adb3a063e9c01b26fe29b5b87f8d0a0db8b7
nicoakuang <thenickdown7@gmail.com>
2024-01-05 11:00:53 +0800

    HW1 Success

--- Assignment/HW1.js
+++ Assignment/HW1.js
@@ -1,19 +1,34 @@
-import { Application } from "https://deno.land/x/oak/mod.ts";
 //by github.com/nicoakuang
+
+//import the module
+import { Application } from "https://deno.land/x/oak/mod.ts";
+
+// Create an instance of the Oak Application
 const app = new Application();
 
+// Define middleware to handle requests
 app.use((ctx) => {
-  let pathname = ctx.request.url.pathname
-  ctx.response.type = 'text/html'
+  // Extract the path from the request URL
+  let pathname = ctx.request.url.pathname;
+
+  // Set the response content type to HTML
+  ctx.response.type = 'text/html';
+
+  // Handle different paths and provide appropriate responses or redirects
   if (pathname.startsWith("/nqu/csie/")) {
+    // Respond with a link to National Quemoy University Department of Computer Science and Information Engineering
     ctx.response.body = `<a href="https://csie.nqu.edu.tw//">National Quemoy University Department of Computer Science and Information Engineering</a>`;
   } else if (pathname.startsWith("/nqu/")) {
+    // Respond with a link to National Quemoy University
     ctx.response.body = `<a href="https://www.nqu.edu.tw/">National Quemoy University</a>`;
   } else if (pathname.startsWith("/to/nqu/csie/")) {
+    // Redirect to National Quemoy University Department of Computer Science and Information Engineering
     ctx.response.redirect('https://csie.nqu.edu.tw//');
   } else if (pathname.startsWith("/to/nqu/")) {
+    // Redirect to National Quemoy University
     ctx.response.redirect('https://www.nqu.edu.tw/');
   } else {
+    // For any other path, display information about available routes
     ctx.response.body = `
     <html>
     <body>
@@ -26,6 +41,10 @@ app.use((ctx) => {
     `;
   }
 });
+
+// Start the application on port 8000
+await app.listen({ port: 8000 });
+
 
 console.log('Started at: http://127.0.0.1:8000')
 await app.listen({ port: 8000 });
---
+++ Assignment/readme.md
@@ -0,0 +1,8 @@
+資工二 111110534 鄭石光
+Thanks chen teacher, for sharing your knowledge, i learn so much
+
+Assignment 1 #Success
+finish method, All assignments are completed after understanding the teacher's program and modifying the teacher's program into the homework requirements.
+I remember, teacher also help me when that time, teacher said "http://127.0.0.1:8000/nqu/" not "http://127.0.0.1:8000/nqu"
+Link below:
+Source: https://github.com/nicoakuang/ws112a/blob/master/Assignment/HW1.js
