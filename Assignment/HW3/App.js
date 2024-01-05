dccd3bc0d07b82cd715990845dbaea99aca36899
nicoakuang <thenickdown7@gmail.com>
2024-01-05 11:16:33 +0800

    HW3 Success

--- Assignment/HW3/App.js
+++ Assignment/HW3/App.js
@@ -1,15 +1,19 @@
-// App.js
 //by github.com/nicoakuang
 
+// Import the required modules
 import { Application, Router } from "https://deno.land/x/oak/mod.ts";
-import * as render from './Render.js'
+import * as render from './Render.js';
 
+// Define an array of posts
 const posts = [
   { id: 0, title: 'Li He Wei', body: '0909116804' },
   { id: 1, title: 'Kelly', body: '0910111251' }
 ];
 
+// Create a router instance
 const router = new Router();
+
+// Define routes and their handlers
 router
   .get('/', list)
   .get("/search/search", search)
@@ -18,18 +22,24 @@ router
   .post("/search", find)
   .post('/post', create);
 
+// Create an application instance
 const app = new Application();
 app.use(router.routes());
 app.use(router.allowedMethods());
 
+// Route Handlers
+
+// List all posts
 async function list(ctx) {
   ctx.response.body = await render.list(posts);
 }
 
+// Display the form for adding a new post
 async function add(ctx) {
   ctx.response.body = await render.newPost();
 }
 
+// Display a specific post
 async function show(ctx) {
   const id = ctx.params.id;
   const post = posts[id];
@@ -37,23 +47,25 @@ async function show(ctx) {
   ctx.response.body = await render.show(post);
 }
 
+// Display the search form
 async function search(ctx) {
   ctx.response.body = await render.search();
 }
 
+// Search for a post based on the submitted form data
 async function find(ctx) {
-  const body = ctx.request.body()
+  const body = ctx.request.body();
   if (body.type === "form") {
-    const pairs = await body.value
+    const pairs = await body.value;
     let name, number;
     for (let i of pairs)
-      name = i
+      name = i;
     const found = posts.some(post => post.title === name[1]);
 
     if (found) {
       for (let i of posts) {
         if (i.title == name[1])
-          number = i.body
+          number = i.body;
       }
       ctx.response.body = await render.found(name[1], number);
     } else {
@@ -62,15 +74,16 @@ async function find(ctx) {
   }
 }
 
+// Create a new post based on the submitted form data
 async function create(ctx) {
-  const body = ctx.request.body()
+  const body = ctx.request.body();
   if (body.type === "form") {
-    const pairs = await body.value
-    const post = {}
+    const pairs = await body.value;
+    const post = {};
     for (const [key, value] of pairs) {
-      post[key] = value
+      post[key] = value;
     }
-    console.log('post=', post)
+    console.log('post=', post);
     const id = posts.push(post) - 1;
     post.created_at = new Date();
     post.id = id;
@@ -78,5 +91,6 @@ async function create(ctx) {
   }
 }
 
-console.log('Server is running at http://127.0.0.1:8000')
+// Start the application and listen on port 8000
+console.log('Server is running at http://127.0.0.1:8000');
 await app.listen({ port: 8000 });
